cf9bb1b716c2bbf830f6313596ee73b45a6aae13
Kübra <kubranur216@gmail.com>
2023-01-05 13:42:37 +0300

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
+var $cont = document.querySelecto('.cont');
+var $elsArr = [].slice.call(document.querySelector('.el'));
+var $closeBtn = [].slice.call(document.querySelector('.el__close-btn'));
 
+setTimeout(function() {
+    $cont.classList.remove('s--inactive')
+},200);
+
+
+$elsArr.forEach(function($el) {
+    $el.addEventListener('click',function(){
+        if (this.classList.contains('s--active')) return;
+        $cont.classList.add('s--el-active');
+        this.classList.add('s--active');
+    });
+    
+});
+
+    $closeBtnsArr.forEach(function($Btn){
+       $btn.addEventListener('click',function(e) {
+       e.stopPropagation();
+       $cont.classList.remove('s--el-active');
+       document.querySelector('.el.s--active').classList.remove('s--active');
+       }); 
+    });
+    
